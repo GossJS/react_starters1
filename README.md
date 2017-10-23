60c9427eec0718d26bb9546336edf1308e0bc78d
gossoudarev <igossoudarev@herzen.spb.ru>
2017-10-24 01:29:50 +0300

    public fields forever

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # react_starters1
 gentle introduction to react
+
+in this branch we have
+
+`onButtonClick = e=>{
+  alert(e.target.nodeName);
+}`
+
+and
+
+`<button onClick={this.onButtonClick}>Click me!</button>`
+
+and use babel-preset-stage-0 here
+
+because it's public fields proposal
+
+https://github.com/tc39/proposal-class-fields
+
+Arrow function preserves context in which it was defined and thus we set handler directly as a class field
--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -2,20 +2,20 @@ import React, {Component as Comp} from 'react';
 import {interval as inter} from 'gossrandom';
 const n = inter(40, 60);
 
-export default class Greeting extends Comp {
+export default class extends Comp {
   constructor(props) {
     super(props);
     this.state = {
       text: ''
     }
   }
-  onButtonClick(e){
+  onButtonClick = e=>{
     alert(e.target.nodeName);
   }
   render() {
     return <div><hr/>
       <h2>Hi! You random number is {n}!</h2>
-      <button onClick={e=>this.onButtonClick(e)}>Click me!</button>
+      <button onClick={this.onButtonClick}>Click me!</button>
     </div>
   }
 }
