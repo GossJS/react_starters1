b1c4fd9bb5da1b574413fb009b77063f48e0f638
EliasGoss <ibg2003@yandex.ru>
2017-11-13 03:16:25 +0300

    linting

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,9 +1,6 @@
 import React, { Component as Comp } from 'react';
-import { interval as inter } from 'gossrandom';
 import { get } from 'axios';
 import 'babel-polyfill';
-
-const n = inter(40, 60);
 
 export default class extends Comp {
   constructor(props) {
@@ -25,7 +22,7 @@ export default class extends Comp {
   render() {
     return (<div><hr />
       <h2>{this.state.text}</h2>
-      <button style={{ background:'silver' }} onClick={::this.onButtonClick}>{this.props.title}</button>
+      <button style={{ background: 'aqua' }} onClick={::this.onButtonClick}>{this.props.title}</button>
       <h2>Логины пользователей:</h2>
       <ul>
          {this.state.users.map((x, i) => (<li key={i}>{x.login}</li>))}
