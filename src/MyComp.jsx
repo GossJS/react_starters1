7b4b93cc6f29c59995d482808b1a3981970d471c
gossoudarev <igossoudarev@herzen.spb.ru>
2017-11-20 03:18:10 +0300

    first step to ssr

---
+++ src/MyComp.jsx
@@ -0,0 +1,28 @@
+import React, { Component as Comp } from 'react';
+
+export default class extends Comp {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'by Goss',
+      maintext: 'Demo react'
+    };
+  }
+  onClick({ target }) {
+    this.setState({ maintext: target.textContent });
+  }
+  get myStyle0() {
+    return { style: { border: 'double 3px silver', borderRadius: '3px' } };
+  }
+  get myStyle() {
+    return { style: { color: 'blue', cursor: 'pointer' } };
+  }
+
+  render() {
+    return (<div {...this.myStyle0}><h1
+                      onClick={e => this.onClick(e)} {...this.myStyle}
+                      title={this.state.title}
+    >{this.props.title}</h1>
+                  <p>{this.props.text}</p><p><i>{this.state.maintext}</i></p></div>);
+  }
+}
--- src/Page2.jsx
+++ src/Page2.jsx
@@ -4,21 +4,18 @@ import { Switch } from 'react-router';
 import { NavLink, Route } from 'react-router-dom';
 import Chips from './Chips';
 import Toggles from './Toggles';
-import MyComp from './MyComp';
 
 const HOME_PAGE_ROUTE = '/';
-const MYCOMP_PAGE_ROUTE = '/mycomp';
 const CHIPS_PAGE_ROUTE = '/chips';
 const TOGGLES_PAGE_ROUTE = '/toggles';
 
-const Home = () => <h2 className="jumbotron">Your choice is?</h2>;
+const Home = () => <h2 className="jumbotron">Your choice?</h2>;
 
 const Nav = () =>
   <nav className="bg-light" style={{ borderRadius: '10px' }}>
       <ul>
         {[
           { route: HOME_PAGE_ROUTE, label: 'Go home!' },
-          { route: MYCOMP_PAGE_ROUTE, label: '*See a compo*!' },
           { route: CHIPS_PAGE_ROUTE, label: 'Enjoy chips!' },
           { route: TOGGLES_PAGE_ROUTE, label: 'Toggle like a pro!' },
         ].map(link => (
@@ -38,13 +35,10 @@ export default class extends Comp {
         <div className="col">
           <Switch>
             <Route exact path={HOME_PAGE_ROUTE} component={Home} />
-            <Route exact path={MYCOMP_PAGE_ROUTE} render={() => <MyComp title="hello" text="hey" />} />
-            <Route path={CHIPS_PAGE_ROUTE} component={Chips} />
+            <Route path={CHIPS_PAGE_ROUTE} render={() => <Chips />} />
             <Route path={TOGGLES_PAGE_ROUTE} component={Toggles} />} />
           </Switch>
         </div>
       </div></MuiThemeProvider>);
   }
 }
-
-export { MYCOMP_PAGE_ROUTE };
